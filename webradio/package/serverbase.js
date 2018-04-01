8034ab912d8b625c526fec7b1d34ed8d97f4a3f9
Erik Palsbo <erik@palsbo.com>
2018-04-02 00:41:51 +0200

    errors

---
+++ webradio/package/serverbase.js
@@ -0,0 +1,85 @@
+﻿var express = require('express');
+const WebSocket = require('ws');
+const http = require('http');
+const url = require('url');
+var app = express()
+var onlist = {};
+var parmlist = {};
+var wssobj;
+
+var _options = {
+    doc: '/index.html',
+    dir: __dirname,
+    port: 80,
+    url: null
+}
+
+//  todo!   must be implemented in class
+function checkArgs(req, res) {
+    for (rid in req.query) {
+        for (id in parmlist) {
+            if (id == rid) parmlist[id](req.params[0], req.query[rid]);
+        }
+    }
+}
+
+app.get(/^(.+)$/, function (req, res) {
+    if (req.params[0] == '/') req.params[0] = _options.doc;
+    checkArgs(req, res);
+    res.sendFile(_options.dir + req.params[0]);
+});
+
+function handleStream(ar) {
+    for (id in ar) {
+        for (on in onlist) {
+            if (id == on) onlist[on](ar[id])
+        }
+    }
+}
+
+class serverbase {
+    constructor(options) {
+        if (options != undefined) {
+            console.log()
+            for (var opt in options) {
+                _options[opt] = options[opt];
+            }
+        }
+        const server = http.createServer(app);
+        var wss;
+        if (!_options.url) wss = new WebSocket.Server({ server });
+        else {
+            var ar = _options.url.split(':');
+            console.log(ar);
+            wss = new WebSocket.Server({ server: ar[0], port: ar[1]});
+        }
+        wssobj = wss;
+        wss.on('connection', function connection(ws, req) {
+            handleStream({ 'connect': ws })
+            ws.on('message', function incoming(data) {
+                handleStream(JSON.parse(data));
+            });
+        });
+        server.listen(_options.port, function () {
+            handleStream({"listen": _options.port})
+        })
+    }
+
+    on (id, func) {
+        onlist[id] = func;
+    }
+
+    onparm(id, func) {
+        parmlist[id] = func;
+    }
+
+    broadcast (data) {
+        wssobj.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    };
+}
+
+module.exports = serverbase;
--- webradio/package/webradio.js
+++ webradio/package/webradio.js
@@ -1,78 +1,72 @@
-var express = require('express');
-const WebSocket = require('ws');
-const http = require('http');
-const url = require('url');
+/*
+  options for constructor:
+    port: webserver port (default 80)
+    url: <url for websocket server incl. port ('localhost:81')
+    OBS! if url is omitted (default), websocket will be on same location and port as webserver
+    dir: <base folder (relative to server folder) where webpages are located. default __dirname (server folder)
+    doc: document to be used, if no document is givne in web-url. default is '/index.html'
+
+    if the web url contains parameters, an event can be raised for each value.
+    giving the url: http://server/index.html?name=erik the name will raise the following callback
+    <serverbase obj>.onparm('name', func) will call the function 'func(page, value)' wher page is the name of the requested page, value is the value of the parameter
+    data from web-socket client is in JSON format;
+
+    data from websocket client is expected as a JSON msg. ex. "{'url': 'http://myserver.com'}"
+    to register a callback for 'url' do this:
+    <serverbase obj>.on('url', func) wher func is function func(data) {} (data is the value of 'url')
+    
+*/
+const serverbase = require("./serverbase");
 const radio = require("radio-stream");
-var app = express()
-//var stream;
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+var ww = new serverbase({ port: 81, dir: __dirname + '/web' });
 
-var currentUrl = "http://live-icy.gss.dr.dk:80/A/A02H.mp3";
+var currentUrl;
 var currentTitle = "";
 var currentInfo = "";
 
-function checkArgs(req, res) {
-    switch (req.params[0]) {
-        case '/index.html':
-            if (req.query.id) {
-                console.log("Found %s in %s", req.query.id, req.params[0])
-            }
-            break;
-    }
+String.prototype.replaceall = function (a, b) {
+    var s = this;
+    while (s.indexOf(a) >= 0) s = s.replace(a, b);
+    return s;
 }
 
-app.get(/^(.+)$/, function (req, res) {
-    if (req.params[0] == '/') req.params[0] = '/index.html';
-    checkArgs(req, res);
-    res.sendFile(__dirname + '/web' + req.params[0]);
-});
-
-wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
+function bcurl(data) {
+    currentUrl = data;
+    currentTitle = "";
+    currentInfo = "";
+    console.log("Now broadcasting url:", currentUrl);
+    ww.broadcast(JSON.stringify({ 'url': currentUrl, 'title': currentTitle, 'info': currentInfo }));
+    var stream = new radio.createReadStream(currentUrl)
+    stream.on("error", function () {
+        currentInfo = "Stream error!";
+        ww.broadcast(JSON.stringify({ 'info': currentInfo }));
+    })
+    stream.on("connect", function () {
+        currentInfo = stream.headers['ice-audio-info'].replaceall(';', '<br/>');
+        ww.broadcast(JSON.stringify({ 'info': currentInfo }));
+        stream.on("metadata", function (title) {
+            currentTitle = title.toString().replace('StreamTitle=', '').replace(';StreamUrl=\'\';', '');
+            if (currentTitle.indexOf('\'') == 0) currentTitle = currentTitle.substr(1);
+            if (currentTitle.lastIndexOf('\'') < currentTitle.length - 3) currentTitle = currentTitle.substr(0, currentTitle.lastIndexOf('\''));
+            if (currentTitle == '\';') currentTitle = '';
+            ww.broadcast(JSON.stringify({ 'title': currentTitle }));
+        });
     });
-};
-
-handleStream = function (ar) {
-    for (id in ar) {
-        switch (id) {
-            case 'url':
-                currentUrl = ar.url;
-                currentTitle = "";
-                currentInfo = "";
-                wss.broadcast(JSON.stringify({ 'url': currentUrl, 'title': currentTitle, 'info':currentInfo }));
-                var stream = new radio.createReadStream(currentUrl)
-                stream.on("error", function () {
-                    currentInfo = "Stream error!";
-                    wss.broadcast(JSON.stringify({ 'info': currentInfo }));
-                })
-                //stream.on("close", function() { console.log("Stream Closed")});
-                stream.on("connect", function () {
-                    currentInfo = stream.headers['ice-audio-info'];
-                    wss.broadcast(JSON.stringify({ 'info': currentInfo }));
-                    stream.on("metadata", function (title) {
-                        currentTitle = title.toString().replace("StreamTitle=", "");
-                        wss.broadcast(JSON.stringify({ 'title': currentTitle }));
-                    });
-                });
-                break;
-        }
-    }
 }
 
-wss.on('connection', function connection(ws, req) {
-    ws.send(JSON.stringify({'url':currentUrl, 'title':currentTitle, 'info':currentInfo}));
-    ws.on('message', function incoming(data) {
-        handleStream(JSON.parse(data));
-    });
-});
+ww.onparm('url', function (doc, data) {     //  action if url contains parameter 'url' returns document requested and value of parameter
+    console.log("Value of parmeter 'url' in page '%s' is '%s'", doc, data);
+})
 
-handleStream({ 'url': currentUrl });
+ww.on('url', bcurl);    //  action if websocket receives 'url' parameter.
 
-server.listen(81, function listening() {
-    console.log('Listening on port %d', server.address().port);
-});
+ww.on('connect', function (ws) {    //  action when a web-client is connected. the socket object is returned
+    console.log("Now connected - sending %s", currentUrl);
+    ws.send(JSON.stringify({ 'url': currentUrl, 'info': "Welcome to webradio" }));
+})
+
+ww.on('listen', function (webport) {   //  action when web-server start to listen.
+    console.log('Listening on port %s', webport);
+    bcurl("http://live-icy.gss.dr.dk:8000/A/A04H.mp3");
+})
